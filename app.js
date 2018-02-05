12b0514b2506a4441fc4217162ecb39ea8aa48de
yanxingsong <457179667@qq.com>
2018-02-05 19:33:26 +0800

    轮播图、签到接口完成；首页图片展示区一般

--- app.js
+++ app.js
@@ -1,16 +1,42 @@
 //app.js
 App({
   onLaunch: function () {
+    var that = this;
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-
+    const APP_ID = 'wx75b6f98051d03aa7';//输入小程序appid  
+    var OPEN_ID = ''//储存获取到openid  
+    const APP_SECRET = '9421a8b369d580df2f47e4b59460f0da';//输入小程序app_secret  
+    var SESSION_KEY = ''//储存获取到session_key 
     // 登录
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        wx.request({
+          //获取openid接口  
+          url: 'https://api.weixin.qq.com/sns/jscode2session',
+          data: {
+            appid: APP_ID,
+            secret: APP_SECRET,
+            js_code: res.code,
+            grant_type: 'authorization_code'
+          },
+          method: 'GET',
+          success: function (res) {
+            console.log(res.data)
+            OPEN_ID = res.data.openid;//获取到的openid  
+            SESSION_KEY = res.data.session_key;//获取到session_key  
+            // that.setData({
+            //   openid: res.data.openid.substr(0, 10) + '********' + res.data.openid.substr(res.data.openid.length - 8, res.data.openid.length),
+            //   session_key: res.data.session_key.substr(0, 8) + '********' + res.data.session_key.substr(res.data.session_key.length - 6, res.data.session_key.length)
+            // })
+            that.globalData.openId = res.data.openid.substr(0, 10) + '********' + res.data.openid.substr(res.data.openid.length - 8, res.data.openid.length), OPEN_ID
+          }
+        })
       }
+
     })
     // 获取用户信息
     wx.getSetting({
@@ -32,9 +58,26 @@ App({
         }
       }
     })
+    // wx.request({
+    //   url: commonUrl + "/user/micro/" + OPEN_ID,
+    //   header: { "Content-Type": "application/json" },
+    //   method: "GET",
+    //   success: function (res) {
+    //     sign_state = true,
+    //       that.setData({
+    //       })
+    //   },
+    //   fail: function () {
+
+    //   }
+    // })
   },
+
   globalData: {
     userInfo: null,
-    imageDomain: 'http://47.95.2.132:9999/file/'
+    openId: '',//储存获取到openid  
+    userId: '',//储存获取到userId  
+    commonUrl: 'http://47.95.2.132:9041/',
+    imageDomain: 'http://47.95.2.132:9999/images/'
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -2,166 +2,158 @@
 //获取应用实例
 const app = getApp();
 var imageDomain = app.globalData.imageDomain;
+var commonUrl = app.globalData.commonUrl;
+var openId = app.globalData.openId;
+var sign_state = false;
+var pageIndex = 1;
+var pageSize = 10;
 Page({
   data: {
-    imgUrls: [
-      {
-        id: 'image1',
-        // link: 'https://www.baidu.com/',
-        url: 'http://img04.tooopen.com/images/20130712/tooopen_17270713.jpg'
-        // url: imageDomain+'2.jpg'
-      },
-      {
-        id: 'image2',
-        // link: 'https://www.baidu.com/',
-        url: 'http://img04.tooopen.com/images/20130617/tooopen_21241404.jpg'
-        // url: imageDomain + '3.jpg'
-      },
-      {
-        id: 'image3',
-        // link: 'https://www.baidu.com/',
-        url: 'http://img04.tooopen.com/images/20130701/tooopen_20083555.jpg'
-        // url: imageDomain + '4.jpg'
-      },
-      {
-        id: 'image4',
-        // link: 'https://www.baidu.com/',
-        url: 'http://img02.tooopen.com/images/20141231/sy_78327074576.jpg'
-        // url: imageDomain + '5.jpg'
-      }
-    ],
+    hotImage1: imageDomain + "2343424/93f61c0a31764383a4c19c44278a9a91.png",
+    hotImage2: imageDomain + "2343424/7df0d4f87109460b883dfe5b535406fc.png",
+    hotImage3: imageDomain + "2343424/93f61c0a31764383a4c19c44278a9a91.png",
+    imgUrls: [],
     indicatorDots: true,
     autoplay: true,
     interval: 5000,
     duration: 1000,
+    searchKeyword: '',  //需要搜索的字符  
+    searchSongList: [], //放置返回数据的数组  
+    isFromSearch: true,   // 用于判断searchSongList数组是不是空数组，默认true，空的数组  
+    pageIndex: 1,   // 设置加载的第几次，默认是第一次  
+    pageSize: 10,      //返回数据的个数  
+    searchLoading: true, //"上拉加载"的变量，默认false，隐藏  
+    searchLoadingComplete: true , //“没有数据”的变量，默认false，隐藏  
     pictureList: [
-      {
-        icon: '../../images/image1.jpg'
-      },
-      {
-        icon: '../../images/image2.jpg'
-      },
-      {
-        icon: '../../images/image3.jpg'
-      },
-      {
-        icon: '../../images/image4.jpg'
-      },
-      {
-        icon: '../../images/image4.jpg'
-      },
-      {
-        icon: '../../images/image3.jpg'
-      },
-      {
-        icon: '../../images/image2.jpg'
-      },
-      {
-        icon: '../../images/image1.jpg'
-      },
-      {
-        icon: '../../images/image2.jpg'
-      },
-      {
-        icon: '../../images/image3.jpg'
-      },
-      {
-        icon: '../../images/image4.jpg'
-      },
-      {
-        icon: '../../images/image1.jpg'
-      },
-      {
-        icon: '../../images/image3.jpg'
-      },
-      {
-        icon: '../../images/image4.jpg'
-      },
-      {
-        icon: '../../images/image1.jpg'
-      },
-      {
-        icon: '../../images/image2.jpg'
-      },
-      {
-        icon: '../../images/image3.jpg'
-      },
-      {
-        icon: '../../images/image3.jpg'
-      },
-      {
-        icon: '../../images/image3.jpg'
-      },
-      {
-        icon: '../../images/image4.jpg'
-      },
+
     ],
-    sign_state: false,
+
     sign_icon: '../../images/unsign.png',
   },
 
   onLoad: function () {
+    let that = this;
     let sign_state = this.data.sign_state;
     if (sign_state) {
       this.setData({
         sign_icon: '../../images/sign.png'
       })
     }
+    that.getIndicator();
+    that.getHotImage();
   },
+  //获取轮播图
+  getIndicator: function () {
+    let that = this;
+    wx.request({
+      url: commonUrl + "carouselFigure/micro/1/4",
+      header: { "Content-Type": "application/json" },
+      method: "GET",
+      success: function (res) {
+        console.log(res);
+        that.setData({
+          imgUrls: res.data.data.list,
+        })
+      },
+      fail: function () {
+        console.log('getIndicator--GG');
+      }
+    })
+  },
+  //top
   toTop: function () {
     wx.navigateTo({
       url: '../top/top',
     })
   },
+  //百科（跳搜索）
   toSearch: function () {
     wx.navigateTo({
       url: '../search/search',
     })
   },
+  //花（商城，暂未开放）
+  toFlower: function () {
+    wx.showModal({
+      content: '商城暂未开放',
+      showCancel: false,
+      confirmColor: "#000000",
+      success: function (res) {
+        if (res.confirm) {
+          console.log('用户点击确定')
+        } else {
+          console.log('用户点击取消')
+        }
+      }
+    })
+  },
+  //签到
   sign: function () {
-    let sign_state = this.data.unsign_state;
+    let that = this;
+    // url: commonUrl + "userSign/micro/" + openId;
     if (!sign_state) {
-      this.setData({
-        // unsign_state: false
-        sign_icon: '../../images/sign.png'
+      wx.request({
+        url: commonUrl + "userSign/micro/" + app.globalData.openId,
+        header: { "Content-Type": "application/json" },
+        method: "GET",
+        success: function (res) {
+          sign_state = true,
+            that.setData({
+              sign_icon: '../../images/sign.png'
+            })
+          wx.showToast({
+            title: '签到成功',
+            duration: 1000,
+            mask: true
+          })
+        },
+        fail: function () {
+          wx.showToast({
+            title: '签到失败',
+            duration: 1000,
+            mask: true
+          })
+        }
       })
     }
   },
-
-  // test: function () {
-  //   wx.request({
-  //     url: "http://4795.2.132:9041/encyclopedia/micro/addEncyclopedi",
-  //     header: {
-  //       "Content-Type": "application/json"
-  //     },
-  //     method: "POST",
-
-  //     success: function (res) {
-
-  //     },
-  //     fail: function () {
-
-  //     }
-  //   }),
-  //  },
-  test: function () {
+  //热区图片
+  getHotImage: function () {
+    let that = this;
     wx.request({
-      url: "http://47.95.2.132:9041/encyclopedia/micro/addEncyclopedia",
+      url: commonUrl + "dynamicImage/micro/hot",
       header: { "Content-Type": "application/json" },
-      method: "POST",
-      data: {
-        author: "1",
-        title: "2",
-        content: "3"
-      },
+      method: "GET",
       success: function (res) {
-        console.log(res);
+        that.setData({
+          // hotImage: res.data.data,
+        })
       },
-      fail: function () { }
-
+    })
+  },
+  //图片展示区图片
+  getImagesList: function () {
+    let that = this;
+    wx.request({
+      url: commonUrl + "dynamicImage/micro/" + pageIndex + "/" + pageSize,
+      header: { "Content-Type": "application/json" },
+      method: "GET",
+      success: function (res) {
+        that.setData({
+          pictureList: res.data.data.list
+        })
+      },
     })
   },
 
+  /**
+ * 页面上拉触底事件的处理函数
+ */
+  onReachBottom: function () {
+    pageIndex++;
+    let that = this;
+    that.getImagesList();
+  },
 }, )
 
 
